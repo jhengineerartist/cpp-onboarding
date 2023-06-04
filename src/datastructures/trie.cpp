ab03ef37d7dc909d6406d2816121278c38d7f82b
= <=>
2023-06-04 14:59:56 -0700

    Fixed shared_ptr and reference issues I introduced

--- src/datastructures/trie.cpp
+++ src/datastructures/trie.cpp
@@ -13,43 +13,43 @@ TrieNode::TrieNode()
 
 void Trie::InsertKey(const string &key)
 {
-    TrieNode &node = root;
+    TrieNode *pNode = &root;
     // Over the range of characters in the key, either nagivate
     // through the tree by ascisigning to pNode, or create a new node
     // if one does not yet exist for a character
     for (char const &c : key)
     {
         int char_index = c - TRIE_FIRST_CHAR;
-        if (node.children[char_index] == nullptr)
+        if (pNode->children[char_index] == nullptr)
         {
-            node.children[char_index] = make_shared<TrieNode>();
-            node.is_end_of_word = false;
+            pNode->children[char_index] = make_shared<TrieNode>();
+            pNode->is_end_of_word = false;
         }
-        node = *node.children[char_index];
+        pNode = pNode->children[char_index].get();
     }
 
     // node will now be at the end of an iteration, so it points to a word.
-    node.is_end_of_word = true;
+    pNode->is_end_of_word = true;
 }
 
 bool Trie::Search(const string &key)
 {
     bool contains_key = true;
-    TrieNode &node = root;
+    TrieNode *pNode = &root;
     // Over the range of characters in the key, either nagivate
     // through the tree by ascisigning to pNode, or create a new node
     // if one does not yet exist for a character
     for (char const &c : key)
     {
         int char_index = c - TRIE_FIRST_CHAR;
-        if (node.children[char_index] != nullptr)
+        if (pNode->children[char_index] != nullptr)
         {
-            node = *node.children[char_index];
+            pNode = pNode->children[char_index].get();
         }
         else
         {
             contains_key = false;
         }
     }
-    return (contains_key ? node.is_end_of_word : false);
+    return (contains_key ? pNode->is_end_of_word : false);
 }
